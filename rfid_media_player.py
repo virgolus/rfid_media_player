6345960661f39d8a98913865a6608b0f256f5d54
virgolus <virgolus@gmail.com>
2024-01-19 21:41:16 +0100

    refactoring

--- rfid_media_player.py
+++ rfid_media_player.py
@@ -15,6 +15,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv(CLIENT_ID),
 sp_device_id="653505de96b5a2742cb24e59794b0bdc15024205"
 
 load_dotenv()
+print('[press ctrl+c to end the script]')
 
 def read_rfid():
     rdr = RFID()
@@ -25,30 +26,25 @@ def read_rfid():
     if not error:
         print("Tag rilevato!")
         (error, uid) = rdr.anticoll()
+        keys = list()
         if not error:
             return uid
 
 def get_spotify_devices(sp):
     """ Restituisce un elenco di dispositivi disponibili su Spotify. """
-    devices = sp.devices()
-    return devices['devices']
-
-def play_track_on_device(track_uri, device_id):
-    """ Riproduce una traccia su un dispositivo specifico. """
-    sp.start_playback(uris=[track_uri], device_id=device_id)
-
-def main():
-
-    devices = get_spotify_devices(sp)
 
     print("Dispositivi disponibili:")
+    devices = sp.devices()
     for device in devices:
         print(f"ID: {device['id']}, Nome: {device['name']}, Tipo: {device['type']}, Volume: {device['volume_percent']}%")
+    return devices['devices']
 
-    track_uri = 'spotify:track:403vzOZN0tETDpvFipkNIL'
+def play_track_on_device(track_uri):
+    """ Riproduce una traccia su un dispositivo specifico. """
+    sp.start_playback(uris=[track_uri], sp_device_id)
+    sp.volume(10, sp_device_id)
 
-    sp.volume(10, device_id=sp_device_id)
-    play_track_on_device(track_uri, sp_device_id)
+def main():
 
     try:
         while True:
@@ -60,11 +56,15 @@ def main():
                 track_uri = f"spotify:track:{''.join(str(i) for i in uid)}"
 
                 # Riproduce la traccia su Spotify
-                sp.start_playback(uris=[track_uri])
+                track_uri = 'spotify:track:403vzOZN0tETDpvFipkNIL'
+                play_track_on_device(track_uri)
 
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Script interrotto")
+
+   finally:
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
